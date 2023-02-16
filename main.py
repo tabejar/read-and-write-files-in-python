716505aa50df3c6a7ea4b960ede19c4e0ad8d673
Thomas Bejar <113930376+tabejar@users.noreply.github.com>
2023-02-16 18:40:28 +0000

    learned to read, write, and append txt files

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
-# remember to fork this and push to github
+# #File Objects
 
+# f = open('test.txt', 'a')
+# f.write(" I like garfield")
+# #read gives us everything
+# #readline gives us the first line
+# # print(f.readline())
+
+# f.close()
+
+# f = open('employees.txt', 'a')
+# f.write(" These are very boring names")
+# f.write(" I like pie")
+# f.close()
+
+# with open('test.txt', 'w') as f:
+  # f_contents = f.read()
+  # f.write("wowie fortnite")
+  # print(f)
+
+# with open('test.txt', 'w') as f:
+#   for line in f:
+#     print(line, end='')
+
+with open('period7.txt','a') as f:
+  f.write(" I know how to read, write, and append text files on python")
